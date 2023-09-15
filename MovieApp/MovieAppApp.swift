3cc5fa47e07a160dd8561cbe13bbaa64cca116a2
Danila Gorbunov <danone14@yandex.ru>
2023-09-15 13:41:33 +0300

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Dan Gorb on 15.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
